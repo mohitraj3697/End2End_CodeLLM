97505f2b36518080dce129d39b102d4268db0827
Mohit Raj <mohitraj3697@gmail.com>
2026-01-09 02:11:15 +0530

    tokeniser

---
+++ tokeniser.py
@@ -0,0 +1,90 @@
+with open("ai.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+import re
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+
+
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+    
+
+
+
+# if unknown token is present in the vocab, use it for unknown words
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}   # updated vocab with special tokens
+
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+    
+
+
+
+
+
+
+tokenizer = SimpleTokenizerV2(vocab)
+
+text1 = "From a psychological perspective, the term hallucination can be misleading because it suggests similarity to human mental phenomena."
+text2 = "Artificial Intelligence."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(text)
+
+
+
+print(tokenizer.encode(text))
+
+print(tokenizer.decode(tokenizer.encode(text)))
+
